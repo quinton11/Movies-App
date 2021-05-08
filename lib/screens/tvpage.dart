1b1f962b5de249d70b9fb84de5d5d0523aff3da4
quinton11 <quintons831@gmail.com>
2021-05-08 23:46:17 +0100

    Done with tv page

--- lib/screens/tvpage.dart
+++ lib/screens/tvpage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -108,10 +110,165 @@ class TvPage extends StatelessWidget {
                     )
                   ],
                 ),
-              )
+              ),
+              SizedBox(height: 20),
+              TvScrollList(
+                width: width,
+                datalist: popular,
+                title: 'Popular',
+              ),
+              TvScrollList(
+                width: width,
+                datalist: airingtoday,
+                title: 'Airing Today',
+              ),
+              TvScrollList(
+                width: width,
+                datalist: toprated,
+                title: 'Top Rated',
+              ),
+              TvScrollList(
+                width: width,
+                datalist: ontheair,
+                title: 'On The Air',
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TvScrollList extends StatelessWidget {
+  const TvScrollList({
+    Key key,
+    @required this.width,
+    this.datalist,
+    this.title,
+  }) : super(key: key);
+
+  final double width;
+  final datalist;
+  final title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //padding: EdgeInsets.all(5),
+      height: 300,
+      width: width,
+      decoration: BoxDecoration(
+        color: Colors.black,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              color: Colors.amber,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 230,
+            child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => Container(
+                height: 230,
+                width: 130,
+                decoration: BoxDecoration(
+                  //color: Colors.green,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      //color: Colors.blue,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                        ),
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(
+                            'https://image.tmdb.org/t/p/w500' +
+                                datalist[index].posterpath,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 180,
+                      child: ClipRect(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          child: Container(
+                            height: 50,
+                            width: 130,
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(0, 0, 0, 0),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  height: 20,
+                                  width: 130,
+                                  child: Text(
+                                    datalist[index].name,
+                                    style: TextStyle(
+                                      color: Colors.white70,
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                    ),
+                                    Text(
+                                      '${datalist[index].voteaverage}',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.white70,
+                                        fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              separatorBuilder: (context, index) => SizedBox(width: 15),
+              itemCount: datalist.length,
+            ),
+          ),
+        ],
       ),
     );
   }
